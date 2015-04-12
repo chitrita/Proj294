406d8e6c5dc4b991301c1113b830b6bc290dae87
David DeTomaso <david.detomaso@s121.Millennium.Berkeley.EDU>
2015-04-11 17:46:28 -0700

    Bug fixes to process_raw

--- process_raw.py
+++ process_raw.py
@@ -11,15 +11,24 @@ import subprocess as sp;
 import random;
 import shutil;
 
+CS_PIPELINE_LOCATION = '/data/yosef/packages/ChIPSeq/preproc/ChIPSeq-QC.py';
+CS_globals = {'__file__':CS_PIPELINE_LOCATION};
+CS_locals = {};
+
+
 #Convert input file to FastQ
 #returns fastq filename
 
 def ChIPSeq_Pipeline(fastq_file):
 
+    if(not os.path.isfile(fastq_file)):
+        print("Error: File does not exist:", fastq_file);
+        return;
+
     outdirectory = os.path.dirname(fastq_file);
     dumpdirectory = outdirectory + os.sep + '%010x' % random.randrange(16**10)
-
-    sys.path.append('/data/yosef/packages/ChIPSeq/preproc');
+    os.makedirs(dumpdirectory);
+    
     
     #mess with arguments
     
@@ -35,16 +44,16 @@ def ChIPSeq_Pipeline(fastq_file):
     sys.argv.append('--out');
     sys.argv.append(dumpdirectory);
     
-    chipseqQC = __import__('ChIPSeq-QC');
+    chipseqQC = execfile(CS_PIPELINE_LOCATION, CS_globals, CS_locals);
     
     #Clean up
     sys.argv = old_args;
-    target_file = 'aligned_sorted_marked_dumplicates.bam';  #Need to check if this is what we want   
-    new_file_name = outdirectory + os.sep + fastq_file.rstrip('.fastq') + '.bam';
+    target_file = 'aligned_sorted_marked_duplicates.bam';  #Need to check if this is what we want   
+    new_file_name = outdirectory + os.sep + os.path.basename(fastq_file).rstrip('.fastq') + '.bam';
     shutil.move(dumpdirectory + os.sep + target_file, new_file_name);
     
-    os.rmdir(dumpdirectory);
-    
+    shutil.rmtree(dumpdirectory);
+ 
     return new_file_name;
     
 
